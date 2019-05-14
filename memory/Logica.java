d395f1f0a13a51cd1165dcbb5dfe6f11d12d1e67
JeffreyMeijer <123974@student.drenthecollege.nl>
2019-05-14 11:43:31 +0200

    Logica.java toegevoegd

---
+++ memory/Logica.java
@@ -0,0 +1,22 @@
+package memory;
+
+public class Logica {
+	private int kolommen;
+	private int rijen;
+	
+	//Getters
+	public int getKolommen() {
+		return kolommen;
+	}
+	public int getRijen() {
+		return rijen;
+	}
+	
+	//Setters
+	public void setKolommen(int kolommen) {
+		this.kolommen = kolommen;
+	}
+	public void setRijen(int rijen) {
+		this.rijen = rijen;
+	}
+}
