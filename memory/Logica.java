14d7b052e5463fad245520ecfca61dc3608031ca
JeffreyMeijer <123974@student.drenthecollege.nl>
2019-05-20 10:57:31 +0200

    Als de buttons niet correct kunnen worden aangemaakt, sluit programma (Closes #5)

--- memory/Logica.java
+++ memory/Logica.java
@@ -1,52 +1,52 @@
 package memory;
-
+ 
 import java.io.File;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-
+ 
 public class Logica {
-	private int kolommen;
-	private int rijen;
-	private ImageIcon[][] fotoArray;
-	
-	//Getters
-	public int getKolommen() {
-		return kolommen;
-	}
-	
-	public int getRijen() {
-		return rijen;
-	}
-	
-	public ImageIcon[][] getFotoArray() {
-		return fotoArray;
-	}
-	
-	//Setters
-	public void setKolommen(int kolommen) {
-		this.kolommen = kolommen;
-	}
-	
-	public void setRijen(int rijen) {
-		this.rijen = rijen;
-	}
-	
-	public void setFotoArray(int rijen, int kolommen) {
-		File dir = new File("fotos/card_fronts");
-		File[] files = dir.listFiles();
-		int fileNumber = 0;
-		ImageIcon[][] fotos = new ImageIcon[rijen][kolommen];
-		for(int r = 0; r < rijen; r++) {
-			for (int i = 0; i < kolommen / 2; i++, fileNumber++) {
-				File file = files[fileNumber];
-				for(int k = 0; k < 2; k++) {
-					fotos[r][i + k] = new ImageIcon("" + file);
-					System.out.println(fotos[r][i + k]);
-				}
-			}
-		}
-		this.fotoArray = fotos;
-	}
-	
-	
+    private int kolommen;
+    private int rijen;
+    private ImageIcon[][] fotoArray;
+   
+    //Getters
+    public int getKolommen() {
+        return kolommen;
+    }
+   
+    public int getRijen() {
+        return rijen;
+    }
+   
+    public ImageIcon[][] getFotoArray() {
+        return fotoArray;
+    }
+   
+    //Setters
+    public void setKolommen(int kolommen) {
+        this.kolommen = kolommen;
+    }
+   
+    public void setRijen(int rijen) {
+        this.rijen = rijen;
+    }
+   
+    public void setFotoArray(int rijen, int kolommen) {
+        File dir = new File("src/memory/fotos/card_fronts");
+        File[] files = dir.listFiles();
+        int fileNumber = 0;
+        ImageIcon[][] fotos = new ImageIcon[rijen][kolommen];
+        for(int r = 0; r < rijen; r++) {
+            for (int i = 0; i < kolommen / 2; i++, fileNumber++) {
+                File file = files[fileNumber];
+                for(int k = 0; k < 2; k++) {
+                    fotos[r][i + k] = new ImageIcon("" + file);
+                    System.out.println(fotos[r][i + k]);
+                }
+            }
+        }
+        this.fotoArray = fotos;
+    }
+   
+   
 }
--- memory/Scherm.java
+++ memory/Scherm.java
@@ -1,37 +1,43 @@
 package memory;
-
+ 
 import java.awt.GridLayout;
+import java.io.File;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
+ 
 public class Scherm extends JFrame {
-	public JFrame f;
-	public JPanel p;
-	public void createWindow(int h, int w) {
-		f = new JFrame("Memory");
-		p = new JPanel();
-		p.setLayout(new GridLayout(4,4));
-		f.setVisible(true);
-		f.add(p);
-		f.setSize(h,w);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-	
-	//Maakt buttons
-	public void createButtons(int rijen, int kolommen, int h, int w) {
-		JButton[][] buttons = new JButton[rijen][kolommen];
-		for(int r = 0; r < rijen; r++) {
-			for(int k = 0; k < kolommen; k++) {
-				buttons[r][k] = new JButton(new ImageIcon("fotos/cover/cover.jpg"));
-				p.add(buttons[r][k]);
-			}
-		}
-		f.setSize(h,w + 1);
-		f.setSize(h,w);
-		
-	}
-	
+    public JFrame f;
+    public JPanel p;
+    public void createWindow(int h, int w) {
+        f = new JFrame("Memory");
+        p = new JPanel();
+        p.setLayout(new GridLayout(4,4));
+        f.setVisible(true);
+        f.add(p);
+        f.setSize(h,w);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+   
+    //Maakt buttons
+    public void createButtons(int rijen, int kolommen, int h, int w) {
+        JButton[][] buttons = new JButton[rijen][kolommen];
+        for(int r = 0; r < rijen; r++) {
+            for(int k = 0; k < kolommen; k++) {
+                buttons[r][k] = new JButton(new ImageIcon("src/memory/fotos/cover/cover.jpg"));
+                p.add(buttons[r][k]);
+                //Checkt of de button een parent heeft (in dit geval of hij in een panel zit, zo niet sluit hij de programma)
+                if(buttons[r][k].getParent() == null) {
+                	f.dispose();
+                	System.exit(0);
+                }
+            }
+        }
+        f.setSize(h,w + 1);
+        f.setSize(h,w);
+       
+    }
+    
 }
